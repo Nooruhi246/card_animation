ad79343cdf6fce83289ac683b5775513ad52bd28
Noorruhi246 <ruhitcse.rymec@gmail.com>
2022-04-06 21:33:52 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { motion, AnimatePresence } from 'framer-motion';
+import React, { useState } from "react";
 
 function App() {
+
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <motion.div
+        transition={{ layout: { duration: 1, type: "spring" } }}
+        layout onClick={() => setIsOpen(!isOpen)}
+        className="card"
+        style={{ borderRadius: "1rem" }}>
+        <motion.h2 layout="position"> Frame Work Motion </motion.h2>
+        <AnimatePresence>
+          {isOpen && (
+            <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} className="expand">
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste ex veritatis quaerat ipsum enim non alias ipsam quibusdam facere voluptates.</p>
+              <p>Lorem ipsum dolor sit amet.</p>
+              <button>Hello</button>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </motion.div>
+    </div >
   );
 }
 
